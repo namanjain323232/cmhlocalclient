6b870f016d503bd6ae6a5ff4a59c45cc69ed496c
namanja <naman.na.jain@oracle.com>
2023-02-21 00:11:50 +0530

    Order List

--- src/components/cards/ShowPaymentInfo.js
+++ src/components/cards/ShowPaymentInfo.js
@@ -1,20 +1,29 @@
 import React from "react";
 
-
-const ShowPaymentInfo= ({order}) => {
+const ShowPaymentInfo = ({ order }) => {
   return (
-      <div>
-         <p className="mb-1">
-          <span className= "mr-2 h6">Customer Id: {order.session.customer}</span>{"  "}
-           <span className= "mr-2 font-weight-bold h6">Amount: 
-                 {order.session.currency.toUpperCase()}   {(order.session.amount_total * 100)
-                           }</span> {"  "}            
-            <span className="  mr-2 h6">Payment Method: {order.session.payment_method_types[0]}</span>{" "}
-            <span className="  mr-2 h6">Payment Status: {order.session.payment_status.toUpperCase()}</span>{" "}
-                        <span className= "badge-large bg-primary text-white h6">Status: {order.orderStatus}</span>
-        </p> 
-      </div>
-  )
-}
+    <div>
+      <p className="mb-1">
+        <span className="mr-2 h6">Customer Id: {order.session.customer}</span>
+        {"  "}
+        <span className="mr-2 font-weight-bold h6">
+          Amount:
+          {order.session.currency.toUpperCase()}{" "}
+          {order.session.amount_total * 100}
+        </span>{" "}
+        {"  "}
+        <span className="  mr-2 h6">
+          Payment Method: {order.session.payment_method_types[0]}
+        </span>{" "}
+        <span className="  mr-2 h6">
+          Payment Status: {order.session.payment_status.toUpperCase()}
+        </span>{" "}
+        <span className="badge-large bg-primary text-white h6">
+          Status: {order.orderStatus}
+        </span>
+      </p>
+    </div>
+  );
+};
 
 export default ShowPaymentInfo;
--- src/components/user/UserHistory.js
+++ src/components/user/UserHistory.js
@@ -30,8 +30,9 @@ const UserHistory = () => {
 
   const showEachOrder = () =>
     orders.map((order, i) => (
-      <div className="card m-5 p-3" key={i}>
-        <ShowPaymentInfo order={order} />
+      <div className="card m-2" key={i}>
+        <h4 style={{ fontWeight: "600" }}>Order {i + 1}</h4>
+        {/* <ShowPaymentInfo order={order} /> */}
         {showOrdersInTable(order)}
         <div className="row">
           {/* <div className="col">
@@ -45,11 +46,36 @@ const UserHistory = () => {
     <table className="table table-bordered  h6">
       <thead className="thead-light">
         <tr>
-          <th scope="col">Helper Name </th>
-          <th scope="col">Sub Category </th>
-          <th scope="col">Price </th>
-          <th scope="col">PriceType </th>
-          <th scope="col">Count </th>
+          <th
+            scope="col"
+            style={{ backgroundColor: "#1890FF", color: "white" }}
+          >
+            Helper Name{" "}
+          </th>
+          <th
+            scope="col"
+            style={{ backgroundColor: "#1890FF", color: "white" }}
+          >
+            Sub Category{" "}
+          </th>
+          <th
+            scope="col"
+            style={{ backgroundColor: "#1890FF", color: "white" }}
+          >
+            Amount
+          </th>
+          <th
+            scope="col"
+            style={{ backgroundColor: "#1890FF", color: "white" }}
+          >
+            Payment Status
+          </th>
+          <th
+            scope="col"
+            style={{ backgroundColor: "#1890FF", color: "white" }}
+          >
+            Payment Method
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -62,13 +88,13 @@ const UserHistory = () => {
               <b>{v.vendor.subcategories[0].name}</b>
             </td>
             <td>
-              <b>{v.vendor.price}</b>
+              <b>${v.vendor.price * v.count}</b>
             </td>
             <td>
-              <b>{v.vendor.pricetype}</b>
+              <b>{order.session.payment_status.toUpperCase()}</b>
             </td>
             <td>
-              <b>{v.count}</b>
+              <b>{order.session.payment_method_types[0].toUpperCase()}</b>
             </td>
           </tr>
         ))}
